30d7239b1439dc908e487e867f6f9ba724514b66
ordoven <tadasknieza@gmail.com>
2019-06-17 13:24:18 +0300

    Added mobile resolution support

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Image, Divider } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
+  const mobile = false;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="image">
+          <Image
+            onClick
+            src={
+              "https://source.unsplash.com/random/" +
+              (mobile ? "720x1280" : "1920x1080")
+            }
+            rounded
+            fluid
+          />
+          <Divider />
+          <Image
+            src="https://images.unsplash.com/photo-1557546883-32d60b7e1c38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            rounded
+            fluid
+          />
+          <Divider />
+        </div>
+        <Button>Click Here</Button>
       </header>
     </div>
   );
