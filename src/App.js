c80dc60bd24eb61b80e4c5dc6209554ffd5ffddd
ordoven <tadasknieza@gmail.com>
2019-06-17 16:01:56 +0300

    Added wallpaper fetch button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
-import { Button, Image, Divider } from "semantic-ui-react";
+import { Button, Image, Divider, Icon } from "semantic-ui-react";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       mobile: false,
+      loading: false,
       responseURL: ""
     };
   }
 
-  componentDidMount() {
+  fetchImage() {
+    this.setState({ loading: true });
     fetch(
       new Request(
         "https://source.unsplash.com/random/" +
@@ -21,7 +23,14 @@ class App extends Component {
     ).then(response => {
       this.setState({ responseURL: response.url });
       console.log(this.state.responseURL);
+      setTimeout(() => {
+        this.setState({ loading: false });
+      }, 2000);
     });
+  }
+
+  componentDidMount() {
+    this.fetchImage();
   }
 
   render() {
@@ -40,7 +49,15 @@ class App extends Component {
             </a>
             <Divider />
           </div>
-          <Button>Click Here</Button>
+          <Button
+            loading={this.state.loading}
+            onClick={this.fetchImage.bind(this)}
+            icon
+            labelPosition="left"
+          >
+            <Icon name="refresh" />
+            New Wallpaper
+          </Button>
         </header>
       </div>
     );
