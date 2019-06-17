4aa3e8a832efd343b424645b10973ae413d1131b
ordoven <tadasknieza@gmail.com>
2019-06-17 14:07:42 +0300

    Successfully capture HTTP response URL

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import { Button, Image, Divider } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+import { Button, Image, Divider } from "semantic-ui-react";
 
-function App() {
-  const mobile = false;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="image">
-          <Image
-            onClick
-            src={
-              "https://source.unsplash.com/random/" +
-              (mobile ? "720x1280" : "1920x1080")
-            }
-            rounded
-            fluid
-          />
-          <Divider />
-          <Image
-            src="https://images.unsplash.com/photo-1557546883-32d60b7e1c38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-            rounded
-            fluid
-          />
-          <Divider />
-        </div>
-        <Button>Click Here</Button>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mobile: false,
+      responseURL: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      new Request(
+        "https://source.unsplash.com/random/" +
+          (this.state.mobile ? "720x1280" : "1920x1080")
+      )
+    ).then(response => {
+      this.setState({ responseURL: response.url });
+      console.log(this.state.responseURL);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="image">
+            <a download href={this.state.responseURL}>
+              <Image
+                size={this.state.mobile ? "medium" : "massive"}
+                src={this.state.responseURL}
+                // centered
+                rounded
+                // fluid
+              />
+            </a>
+            <Divider />
+          </div>
+          <Button>Click Here</Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
